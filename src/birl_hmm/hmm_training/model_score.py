bfcdeb488f7dd52250848275525b5a078caa8092
HongminWu <hongminwu0120@gmail.com>
2018-07-27 22:52:04 +0800

    restore the previous modification

--- src/birl_hmm/hmm_training/model_score.py
+++ src/birl_hmm/hmm_training/model_score.py
@@ -181,14 +181,12 @@ def score(score_metric, model, X, lengths):
 
         score = np.array(score_of_trials).mean()
     elif score_metric == '_score_metric_sum_of_loglik_':
-        try:
-            final_time_step_log_lik = [
-                model.score(X[i:j]) for i, j in hmm_util.iter_from_X_lengths(X, lengths)
-                ]
-            matrix = np.matrix(final_time_step_log_lik)
-            s = matrix.sum()
-        except ValueError:
-            s = -1000000
+        final_time_step_log_lik = [
+            model.score(X[i:j]) for i, j in hmm_util.iter_from_X_lengths(X, lengths)
+        ]
+        matrix = np.matrix(final_time_step_log_lik)
+        s = matrix.sum()
+
         score = -s
 
     else:
